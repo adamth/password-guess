d8dcce18d2fdc759f92821d65601eafcc5c33551
adamth <adam@adamth.com>
2021-03-09 10:40:11 +1100

    Mock fauna client in all tests

--- functions/__tests__/new-password.test.js
+++ functions/__tests__/new-password.test.js
@@ -1,9 +1,22 @@
+const faunadb = require("faunadb");
 const { handler } = require("../new-password");
+
+const mockedClient = {
+  query: () => ({
+    data: {
+      password: "12345678",
+      hint: "87654321",
+    },
+  }),
+};
 
 describe("handler", () => {
   let response;
 
   beforeEach(async () => {
+    faunadb.Client = jest.fn().mockImplementation(() => {
+      return mockedClient;
+    });
     response = await handler();
   });
 
